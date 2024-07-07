969bb7830c83c1df9c8709f491f20fe9da9476bf
Rahul kumar <108k.rahul@gmail.com>
2024-07-07 22:18:31 +0530

    practice ques

---
+++ 4_Loops/Practice/Factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< "Enter number: ";
+    cin>> n;
+    
+    int fact = 1;
+    for(int i=n; i>=1; i--){
+        fact*=i;
+    }
+    cout<<"Factorial: " << fact<< endl;
+    return 0;
+}
