526adb4d11297ef64772e5e6256f2d7439196249
Rahul Kumar <108k.rahul@gmail.com>
2024-07-15 22:33:36 +0530

    Arrays

--- 9_Arrays_!/code.cpp
+++ 9_Arrays_!/code.cpp
@@ -1,22 +1,32 @@
 #include<iostream>
 using namespace std;
 
+/* void READit(int nums[]){ //this funct can't calculate the size of array. Because it only knows about the starting point and not about the ending point.
+// THAT'S WHY WE ALSO NEED TO POIVIDE SIZE OF THE ARRAY TO THE FUNCTION MANUALLY.
+    cout<<"size of nums = " << sizeof(nums); //4, because nums is a pointer, not an array.
+    // int n = sizeof(nums)/sizeof(int);
+    // cout<<n<<endl;
+
+    // for(int i=0; i<n; i++){
+    //     cout<<nums[i]<<" ";
+    // }
+    // cout<<endl;
+}*/
+
+void printArr(int nums[], int n){
+    for(int i=0; i<n; i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
 int main(){
-    int n;
-    cout<<"Enter size of array: ";
-    cin>>n;
+    int arr[] = {1,2,3,4,5};
 
-    int arr[5] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<"size of array = " <<n<<endl;
 
-    // int n = (sizeof(arr)/sizeof(int));
-
-    for(int i=0; i<n; i++){
-        cout<<"Enter value "<<i+1<<": ";
-        cin>> arr[i];
-    }
-    for(int i=0; i<n; i++){
-        cout<< arr[i]<<" ";
-    }
-
+    printArr(arr,n);
     return 0;
 }
