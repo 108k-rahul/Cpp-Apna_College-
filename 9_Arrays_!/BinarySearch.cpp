b3a336ff6db82819b0409ef4240b50c1ca73607e
Rahul Kumar <108k.rahul@gmail.com>
2024-07-17 23:51:29 +0530

    Update BinarySearch.cpp

--- 9_Arrays_!/BinarySearch.cpp
+++ 9_Arrays_!/BinarySearch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binarySearch(int *arr, int n, int key){
     int start = 0, end = n-1;
 
-    while(start<=end){
+    while(start<=end){  //we take '<=' in case there is only one element left in the array. i.e: start = end.
         int mid = (start+end)/2;
 
         if(arr[mid] == key){
