f1ea4843ac2dd70e8caaa1042a78dee80b4b96a6
Rahul Kumar <108k.rahul@gmail.com>
2024-07-24 00:24:34 +0530

    Optamized method for find max SubArray Sum

--- 9_Arrays_!!/MaxSubarrSum/Optamized.cpp
+++ 9_Arrays_!!/MaxSubarrSum/Optamized.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
+void maxSubarraySum(int *arr, int n){
+    int maxSum = INT_MIN;
+    for(int start=0; start<n; start++){ // start = 2
+        int currentSum = 0;
+        for(int end=start; end<n; end++){ // end = 2,3,4,5
+            currentSum += arr[end]; 
+            maxSum = max(currentSum, maxSum);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        }
+        // cout<<endl;
+    }
+    cout<<"max SubArraySum = "<< maxSum;
+}
+
 int main() {
-    // Your code goes here
+    int arr[6] = {2,-3,6,-5,4,2};
+    int n = sizeof(arr)/sizeof(int);
+
+    maxSubarraySum(arr, n);
     
     return 0;
 }
