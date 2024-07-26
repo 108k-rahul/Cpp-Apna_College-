45fbe684f6de348b819f22485ceceed35d5bed5a
Rahul Kumar <108k.rahul@gmail.com>
2024-07-26 23:31:07 +0530

    Bubble Sort

---
+++ 10_Sorting/bubble.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void print(int *arr, int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubble(int *arr, int n){
+    bool isSwap = false;
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<(n-i-1); j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap){
+            break; // => array is already sorted.
+        }
+        isSwap = false; // Reset isSwap for the next pass
+    }
+    print(arr, n); // Ensure print is called after sorting
+}
+
+int main(){
+    int arr[5] = {5, 4, 1, 3, 2};
+    int n = sizeof(arr)/sizeof(int);
+
+    bubble(arr, n);
+    return 0;
+}
