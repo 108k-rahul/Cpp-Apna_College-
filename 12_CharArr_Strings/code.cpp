3ec491208414287d82a736798352a106d2c0c09e
Rahul Kumar <108k.rahul@gmail.com>
2024-08-01 13:11:35 +0530

    Character string

---
+++ 12_CharArr_Strings/code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch = 'k';
+    int pos = ch - 'a'; //index of 'k'
+
+    cout<<(int)ch<<endl;
+    cout<<pos<<endl;
+    return 0;
+}
