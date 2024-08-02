d692af301f6929edb2dae0cc99b3cf19ff8e6cad
Rahul Kumar <108k.rahul@gmail.com>
2024-08-02 23:39:52 +0530

    .

--- 12_CharArr_Strings/code.cpp
+++ 12_CharArr_Strings/code.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+#include <cstring>
 using namespace std;
 
 int main(){
-    char ch = 'k';
-    int pos = ch - 'a'; //index of 'k'
+    // char ch = 'k';
+    // int pos = ch - 'a'; //index of 'k'
 
-    cout<<(int)ch<<endl;
-    cout<<pos<<endl;
+    // cout<<(int)ch<<endl;
+    // cout<<pos<<endl;
+
+
+    char work[10] = {'c', 'o', 'd', 'e', '\0'};
+    cout<<work<<endl;
+    cout<<strlen(work)<<endl;
     return 0;
 }
