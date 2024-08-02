b41b77674b569d956240e82bc4b33c745596701b
Rahul Kumar <108k.rahul@gmail.com>
2024-08-02 23:57:06 +0530

    input using 'cin.getline()' in string arrays

--- 12_CharArr_Strings/code.cpp
+++ 12_CharArr_Strings/code.cpp
@@ -10,8 +10,20 @@ int main(){
     // cout<<pos<<endl;
 
 
-    char work[10] = {'c', 'o', 'd', 'e', '\0'};
-    cout<<work<<endl;
-    cout<<strlen(work)<<endl;
+    // char work[] = {'c', 'o', 'd', 'e', '\0'};
+    // // char work[4] = {'c', 'o', 'd', 'e', '\0'};
+    // cout<<work<<endl;
+    // cout<<strlen(work)<<endl;
+
+    // char word[30];
+    // cin >> word; //will not consider anything after whitespace.
+    // cout<<"your word is: '"<<word<<"'"<<endl;
+    // cout<<"length is: "<<strlen(word)<<endl<<endl;
+
+    char sentence[30];
+    cin.getline(sentence, 30, '*'); //will consider even after whitespace.
+    cout<<"your sentence is: '"<<sentence<<"'"<<endl;
+    cout<<"length is: "<<strlen(sentence)<<endl;
+
     return 0;
 }
