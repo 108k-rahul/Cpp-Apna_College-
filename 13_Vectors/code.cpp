781da6db12a8a71860a0b31fd8252a91b42037f4
Rahul Kumar <108k.rahul@gmail.com>
2024-08-15 22:02:27 +0530

    Add 13_Vectors folder

---
+++ 13_Vectors/code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int arr[100] = {1,2,3,4,5};
+
+    int size;
+    cin >> size;
+
+    int *arr = new int[size];
+    
+    return 0;
+}
+
+// void
