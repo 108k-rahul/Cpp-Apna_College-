a0d6e8515c3c038f313e85e34e9e75952834e7a0
Rahul Kumar <108k.rahul@gmail.com>
2024-09-20 18:49:18 +0530

    .

--- 30_LinkedList/practice.cpp
+++ 30_LinkedList/practice.cpp
@@ -1,56 +1,78 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Node{
 public:
     int data;
     Node* next;
-    
     Node(int val){
         data = val;
         next = NULL;
     }
 };
 
-class List{
+class customlist{
     Node* head;
     Node* tail;
 public:
-    List(){
+    customlist(){
         head = NULL;
-        tail = NULL;   
+        tail = NULL;
     }
-    
-    void pushback(int val){
-        Node* newnode = new Node(val);
-        
-        if(head == NULL){
-            head = tail = newnode;
-        }else{
-            tail->next = newnode;
-            tail = newnode;
-        }
-    }
-    
     void printlist(){
         Node* temp = head;
-        
-        while(head != NULL){
-            cout<< temp->data<<" ";
+        while(temp != NULL){
+            cout<<temp->data<<" ";
             temp = temp->next;
         }
+        cout<<endl;
+    }
+    void insert(int val, int position){
+        Node* newnode = new Node(val);
+
+        Node* temp = head;
+        for(int i=0; i<position-1; i++){
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+
+};
+class List{
+public:
+    list<int> ll;
+
+    void pushback(int val){
+        ll.push_back(val);
+    }
+    void pushfront(int val){
+        ll.push_front(val);
+    }
+    void reverse(list<int>& ll_ref){
+        ll_ref.reverse();
+    }
+    void sort(list<int>& ll_ref){
+        ll_ref.sort();
+    }
+    void popfront(){
+        ll.pop_front();
+    }
+    void popback(){
+        ll.pop_back();
     }
 };
 
 int main(){
-    int n;
-    cin>>n;
-    
     List ll;
-    for(int i=0; i<n; i++){
-        ll.pushback(i);
+    customlist cl;
+
+    int values;
+    for(int i=0; i<5; i++){
+        cin>>values;
+        ll.pushback(values);
+        cl.pushback(values);
     }
-    
-    ll.printlist();
+    cl.printlist();
     return 0;
 }
