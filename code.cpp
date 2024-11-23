d8dece2d8e9a20923bb4a8a6b9a60d3809ea3620
Rahul Kumar <108k.rahul@gmail.com>
2024-11-23 08:11:08 +0530

    pop heap

--- code.cpp
+++ code.cpp
@@ -1,104 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node{
-public:
-    int data;
-    Node* left;
-    Node* right;
-    Node(int data){
-        this->data = data;
-        left = right = NULL;
-    }
-};
-
-static int idx = -1;
-
-Node* buildTree(vector<int> nodes){
-    idx++;
-    if(nodes[idx] == -1){
-        return NULL;
-    }
-    
-    Node* currNode = new Node(nodes[idx]);
-    currNode->left = buildTree(nodes);
-    currNode->right = buildTree(nodes);
-    
-    return currNode;
-}
-
-void preorder(Node* root){
-    if(root == NULL){
-        return;
-    }
-    
-    cout<<root->data<<" ";
-    preorder(root->left);
-    preorder(root->right);
-}
-
-void inorder(Node* root){
-    if(root == NULL){
-        return;
-    }
-    
-    inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
-}
-
-void postorder(Node* root){
-    if(root == NULL){
-        return;
-    }
-    
-    postorder(root->left);
-    postorder(root->right);
-    cout<<root->data<<" ";
-}
-
 int main(){
-    int choice;
-    Node* root = NULL;
-    
-    while(true){
-        cin>>choice;
-        if(choice == 1){
-            int n;
-            cin>>n;
-            vector<int> nodes(n);
-            for(int i; i<n; i++){
-                cin>>nodes[n];
-            }
-            root = buildTree(nodes);
-        }else if (choice == 2) {
-            cout << "Inorder: ";
-            inorder(root);
-            cout << endl;
-        } else if (choice == 3) {
-            cout << "Preorder: ";
-            preorder(root);
-            cout << endl;
-        } else if (choice == 4) {
-            cout << "Postorder: ";
-            postorder(root);
-            cout << endl;
-        } else if (choice == 5) {
-            break;
-        } else {
-            cout << "Invalid choice. Try again." << endl;
-        }
+    priority_queue<int> pq;
+
+    int a;
+    for(int i=0; i<5; i++){
+        cin>>a;
+        pq.push(a);
+    } 
+
+    while(!pq.empty()){
+        pq.top();
+        pq.pop();
     }
-    
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
